ddd573bf7f0a200208da9136672ea3194a7cbf4d
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-09-03 12:23:40 +0330

    get data products

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { fetchData } from "../utils/httpReq.js"
+
+async function render(){
+     const productData=await fetchData()
+     console.log(productData)
+}
+document.addEventListener("DOMContentLoaded",render)
